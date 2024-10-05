f141f4448d5c57cbc32292e379c8f31048bbfd1b
computergeek125 <computergeek125@gmail.com>
2024-10-05 18:25:19 -0500

    Convert to Git: v0.1.0  now supports running against a directory and zipping up all the files itself

--- htmltotxt.py
+++ htmltotxt.py
@@ -1,6 +1,10 @@
+import argparse
 from bs4 import BeautifulSoup
+import pathlib
+import sys
+import zipfile
 
-def transform_html_to_whatsapp(html_file):
+def transform_html_to_whatsapp(html_file, text_file):
     # Read the HTML file
     with open(html_file, 'r', encoding='utf-8') as file:
         html_content = file.read()
@@ -35,10 +39,37 @@ def transform_html_to_whatsapp(html_file):
             whatsapp_chat += whatsapp_message
 
     # Save the transformed chat to a file
-    with open('_chat.txt', 'w', encoding='utf-8') as file:
+    with open(text_file, 'w', encoding='utf-8') as file:
         file.write(whatsapp_chat)
 
-    print('Transformation complete. The WhatsApp chat export is saved as _chat.txt')
+    print(f'Transformation complete. The WhatsApp chat export is saved as {text_file}')
 
 # Usage example
-transform_html_to_whatsapp('messages.html')
+#transform_html_to_whatsapp('messages.html')
+
+def what_zip(text_file, name="Whatsapp Chat - person_name.zip"):
+    with zipfile.ZipFile(pathlib.Path(text_file.parent, name), 'w') as zip_object:
+        zip_object.write(text_file, arcname="_chat.txt")
+        sys.stdout.write(f"ZIP file written to {zip_object.filename}\n")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser_filedir = parser.add_mutually_exclusive_group()
+    parser_filedir.add_argument("-f", "--file", help="Name of a singular file to parse")
+    parser_filedir.add_argument("-d", "--dir",  help="Name of directory of files to parse - will parse every html in this folder")
+    args = parser.parse_args()
+
+    if args.file:
+        input_file = pathlib.Path(args.file)
+        output_file = pathlib.Path(input_file.parent, "_chat.txt")
+        transform_html_to_whatsapp(input_file, output_file)
+        what_zip(output_file)
+
+    else:
+        for input_file in pathlib.Path(args.dir).glob("*.html"):
+            sys.stdout.write(f"Processing {input_file}...\n")
+            output_file = input_file.with_suffix(".txt")
+            transform_html_to_whatsapp(input_file, output_file)
+            zip_int_name = input_file.with_suffix("").name
+            zip_name = f"Whatsapp Chat - {zip_int_name}.zip"
+            what_zip(output_file, name=zip_name)
